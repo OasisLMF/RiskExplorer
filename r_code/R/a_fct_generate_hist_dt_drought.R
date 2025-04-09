ff50c108cd3ee70be0f8b88f77fbda8fbace78bd
Stuart Fraser <stuart@disaster-risk.uk>
2025-04-09 14:14:34 +0100

    Update a_fct_generate_hist_dt_drought.R
    Switch 'full range' and '50% range' field labels

--- r_code/R/a_fct_generate_hist_dt_drought.R
+++ r_code/R/a_fct_generate_hist_dt_drought.R
@@ -4,15 +4,15 @@ generate_hist_dt_drought <- function(table_data, display_var){
     table_data |> 
     dplyr::group_by(Year) |> 
     dplyr::summarise(Average = mean(!!sym(display_var)),
-                     `50% Data Range Low` = min(!!sym(display_var)),
-                     `50% Data Range High` = max(!!sym(display_var)),
-                     `Full Range Low` = 
+                     `Full Range Low` = min(!!sym(display_var)),
+                     `Full Range High` = max(!!sym(display_var)),
+                     `50% Data Range Low` = 
                        quantile(
                          !!sym(display_var), 
                          probs =  0.25,
                          na.rm = TRUE
                        ),
-                     `Full Range High` = quantile(
+                     `50% Data Range High` = quantile(
                        !!sym(display_var), 
                        probs =  0.75,
                        na.rm = TRUE
